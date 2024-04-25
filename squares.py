5486cecd967d2b00906258c46dea40247a318d82
ReinePearlBuchi <165788844+ReinePearlBuchi@users.noreply.github.com>
2024-04-25 07:09:33 +0100

    Add files via upload

---
+++ squares.py
@@ -0,0 +1,9 @@
+def sum_of_squares(lst):
+    total = 0
+    for num in lst:
+        total += num ** 2
+    return total
+
+sample_data = [2, 3, 4, 5, 7]
+result = sum_of_squares(sample_data)
+print(result)  # Output: 103
